9c5acf7ff3ac78eadcebf5a1f4919b74e71a1473
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-09-01 19:06:21 +0530

    added resume download link and resume v2

---
+++ resume/plugins/particles/particles-init.js
@@ -0,0 +1,119 @@
+
+
+
+
+
+
+
+particlesJS("header", {
+  "particles": {
+    "number": {
+      "value": 34,
+      "density": {
+        "enable": true,
+        "value_area": 481.0236182596568
+      }
+    },
+    "color": {
+      "value": "#808fb9"
+    },
+    "shape": {
+      "type": "triangle",
+      "stroke": {
+        "width": 0,
+        "color": "#5469a4"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.10422178395625899,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1.8677775882930938,
+        "opacity_min": 0.19489853095232282,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 8.017060304327615,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 29.234779642848423,
+        "size_min": 1.6241544246026904,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 0,
+      "color": "#51618e",
+      "opacity": 0.1763753266952075,
+      "width": 6.573989449548644
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.603412060865523,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 1202.559045649142,
+        "rotateY": 1523.2414578222467
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
--- src/app/js/app.js
+++ src/app/js/app.js
@@ -146,7 +146,7 @@
     var $typed = $("#typed");
     if ($typed.length) {
       var typed = new Typed("#typed", {
-        strings: ["developer", "freelancer", "marketer", "photographer"],
+        strings: ["Web Developer", "Teenager(17)", "Programmer", "Content writer"],
         loop: true,
         typeSpeed: 70
       });
